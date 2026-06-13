abaaa9af51b88805bbef34971d3f03e554bd5493
Ibnutesfaye <abdurezaktesfaye998@gmail.com>
2026-06-13 09:27:15 +0300

    fix: resolve font warnings in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,18 +69,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="antialiased">{children}</body>
     </html>
   );
